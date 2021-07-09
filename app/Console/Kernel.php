b4bda34d8b4e9c4d7a71914609abe28e2f84a13f
Arr12 <aryo.puruhito12@gmail.com>
2021-07-09 12:07:11 +0700

    add schedule running

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('get:data')->dailyAt('01:00');
+        $schedule->command('get:data')->dailyAt('03:00');
     }
 
     /**
